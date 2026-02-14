77fb5184fc72e87697061357975e31e878d35e77
Jsua3 <jjsua_542@unihumboldt.edu.co>
2026-02-13 19:36:50 -0500

    FIRST COMMIT

---
+++ src/main/java/com/example/quiz_2_iii/Quiz2IiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz_2_iii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2IiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quiz2IiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quiz_2_iii/Quiz2IiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz_2_iii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2IiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
